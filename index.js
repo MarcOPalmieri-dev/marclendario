bfec705e4e8625c8ff5dc61fea85ec7b87a1d21c
Marco Palmieri <marquitopalmieri@gmail.com>
2021-04-11 19:45:50 -0300

    agregado de estilos al finalizar un objetivo

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const buttonProgressBar = document.getElementById("buttonProgressBar");
 const container = document.getElementsByClassName("progress-bar")[0];
 const progress = document.querySelector("#progress-done");
+const img_container = document.getElementById("img-container");
+const goal_container = document.getElementById("section");
 const now = moment();
 const month = (now.daysInMonth());
 
@@ -8,8 +10,7 @@ let pixels = 0;
 let days = month * 2;
 let day_comlpleted = 0;
 let current_day = now.format('D');
-// let next_day = (parseInt(now.format('D'))+1);
-let next_day = 50;
+let next_day = (parseInt(now.format('D'))+1);
 buttonProgressBar.addEventListener("click",function(){
 
     if (current_day === next_day){
@@ -25,7 +26,9 @@ function incressPorcentage(){
     container.style.width = days + "px";
     if(pixels === days){
         swal("¡Felicidades!", "Lograste cumplir con tu objetivo!", "success")
-        // agregar un tick en lugar de la imagen que tiene a la izquierda.
+        img_container.style.backgroundImage = "url('tick.png')";
+        goal_container.style.opacity = 0.5;
+        
     }else{
         pixels+=2;
         progress.style.width = pixels + "px";
