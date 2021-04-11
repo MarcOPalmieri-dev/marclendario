4d75a01ad1143011273bd8a05f210a50df02bb16
Marco Palmieri <marquitopalmieri@gmail.com>
2021-04-11 17:51:20 -0300

    agregado de var rest_days para mejorar barra de progreso

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const buttonProgressBar = document.getElementById("buttonProgressBar");
 const container = document.getElementsByClassName("progress-bar")[0];
 const progress = document.querySelector("#progress-done");
 const now = moment();
+const month = (now.daysInMonth());
 
 let pixels = 0;
-let days = 31 * 2;
+let days = month * 2;
 let day_comlpleted = 0;
 
 let current_day = now.format('D');
 let next_day = (parseInt(now.format('D'))+1);
 
+let rest_days = current_day !=1?month - current_day: month;
+
+console.log(rest_days)
 buttonProgressBar.addEventListener("click",function(){
 
     if (current_day === next_day){
@@ -19,19 +23,20 @@ buttonProgressBar.addEventListener("click",function(){
     }
 });
 
+
 function incressPorcentage(){
     
     container.style.width = days + "px";
     if(pixels === days){
         swal("¡Felicidades!", "Lograste cumplir con tu objetivo!", "success")
-        
+        // agregar un tick en lugar de la imagen que tiene a la izquierda.
     }else{
         pixels+=2;
         progress.style.width = pixels + "px";
         current_day++;
         day_comlpleted++;
-        progress.innerHTML = day_comlpleted + "/" + "31 Días";
-
+        progress.innerHTML = day_comlpleted + "/" + rest_days + " Días";
+        // if = current day = 1 : rest_days == only month (30 / 31); but else, rest_days == days left to finish the month
     }
 }
 
