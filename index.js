6058249ba749a4bed091361894b9616341702222
Marco Palmieri <marquitopalmieri@gmail.com>
2021-04-30 12:17:47 -0300

    estilos yfuncion de obtencion de datos del formulario terminado

--- index.js
+++ index.js
@@ -67,6 +67,14 @@ function incressPorcentage(){
 // crear un objeto (puede ser una clase)que contenga el div sema, y ese objeto contiene a los divs de los dias de esa semana.
 // ese objeto sera guardado en un array de objetos.
 
+const close_button = document.getElementById("close_button");
+const objetive_form = document.getElementById("objetive_form");
+const save_objetive_button = document.getElementById("save_objetive");
+// inputs para obtener datos del formulario de Objetivos
+const input_title = document.getElementById("title");
+const select_icon = document.getElementById("icon");
+const input_time = document.getElementById("time");
+
 class Objetives{
     constructor(){
         this.initialize()   
@@ -74,24 +82,59 @@ class Objetives{
 
     initialize(){
         this.array_objetives = [];
-        this.new_objetive_button = new_objetive;
-        this.addClickEvent()
+        this.objetive_form = objetive_form;
+        this.save_objetive_button = save_objetive_button;
+        this.close_button = close_button;
+        this.input_title = input_title;
+        this.select_icon = select_icon;
+        this.input_time = input_time;
+        this.showObjetiveForm()
     // this.getObjetives() Trae los objetivos que ya estan creados. Metodo que recorre un array de objetos? 
     // this.new_objetive = getElement...   Traemos el boton para crear nuevos objetivos. Luego le agregamos el eventListener con el metodo correspondiente. Va a hacer push al array de objetos de arriba, y tambien se le va a agregar como atributo el metodo del progress bar. El
     }
 
-    addClickEvent(){
-        this.new_objetive_button.addEventListener(click,this.newObjetive.bind(this));
+    showObjetiveForm(){
+        this.newObjetive()
+    }
+
+
+    // addClickEvent(){
+        
+        
+    // }
+
+    removeClickEvent(){
+        this.close_button.removeEventListener('click',this.closeForm.bind(this));
+        this.save_objetive_button.removeEventListener('click',this.saveData())
+        // aplicar este evento cuando se oprima el boton guardar
     }
 
     newObjetive(){
+        this.objetive_form.style.display = "flex";
+        this.close_button.addEventListener('click',this.closeForm.bind(this));
+        this.save_objetive_button.addEventListener('click',()=>this.saveData());
         // le ponemos display block a un formulario emergente. Luego obtenemos los datos del formulario. Y eso lo ponemos en un objeto (quizas sea de la clase objetivo, y le modificamos las propiedades. Una de esas propiedades debe ser el valor de pixels,current-day y esos, para que se guarden los valores y no se reinicie
         // y despues con el metodo getObjetives, recorremos el array y los imprimimos.
     }
+
+    closeForm(){
+        objetive_form.style.display = "none"; 
+    }
+
+    saveData(){
+        let title_data = this.input_title.value;
+        let select_icon = this.select_icon.value;
+        let time_data = this.input_time.value;
+
+        console.log(title_data,select_icon,time_data)
+    }
+    
 }
 
 
 
 function startObjetives(){
     this.objetive = new Objetives();
+    console.log(this.objetive)
 }
+
