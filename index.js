205dffd8a4547d056746179beb6e2c93057b6221
Marco Palmieri <marquitopalmieri@gmail.com>
2021-07-14 15:28:32 -0300

    cambio del >= 90 a >90

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ const now = moment();
 
 function progressBar(obj,time,pixels,day_completed,progress_done,img_container,goal_container,progress_container){
     
-    if(time >=90){
+    if(time >90){
         progress_container.style.width = time * 1 + "px";
         pixels+=1;
     }else{
@@ -223,7 +223,8 @@ function addProgressBar(){
 
         let delete_button = document.getElementById("delete-objetive"+i);
 
-            if(obj.time >=90){
+            // making a conditional to avoid breaking the design with the bar.
+            if(obj.time >90){
                 progress_container.style.width = obj.time * 1 + "px";
 
             }else{
